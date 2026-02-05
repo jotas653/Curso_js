4a3162e9825d85dd6785d2c9431d081f8d0d08d8
João Pedro Moura Barboza <jpmourabarboza10@hotmail.com>
2026-02-05 18:09:14 -0300

    Terminei a aula de array

--- Seccao2-JS-Basico/Aula-29.js
+++ Seccao2-JS-Basico/Aula-29.js
@@ -13,6 +13,15 @@ Para mudar o valor de um array devemos fazer o seguinte: varivel[indice que quer
 E acrescentando um novo valor fazemos o seguinte: variavel[novo indice] = passar o valor desse novo indice
 E para sabermos o tamanho do nosso array fazemos assim: variavel.length
 E uma outra maneira de adicionarmos um valor ao final do nosso array e dessa maneira: variavel.push(novo valor)
+Quando queremos adicionar um valor no começo do array fazemos dessa seguinte maniera: unshift(valor que queremos)
+Com a função pop removemos um valor do final do array fazemos o seguinte: variavel.pop(). E podemos armanezar esse
+valor que estamos removendo em uma variavel
+E com o comando shift() removemos o valor que esta no começo: variavel.shift()
+Quando excluimos um item do array alteramos tambem a sua indexação, para que isso não aconteça devemos usar o delete
+fazendo dessa maneira somente removemos o valor e não o indice
+E com o js nos podemos acessar um array que não existe
+Para fatiarmos um array devemos fazer a mesma coisa que colocamos com a string: variavel.slice(de onde queremos ate onde), mas devemos lembrar de sempre colocar um numero a mais
+Para verificarmos se algo esta na instancia de um array devemos fazer o seguinte: console.log(variavel instanceOf(Array))
 */
 
 // const nome = 'João Pedro Moura' Pegar um indice de uma string
@@ -45,4 +54,43 @@ E uma outra maneira de adicionarmos um valor ao final do nosso array e dessa man
 // console.log(alunos)
 
 //                0         1       2
-const alunos = ['Luiz', 'Maria', 'João']
+// const alunos = ['Luiz', 'Maria', 'João']
+// alunos.unshift('Luiza')
+// alunos.unshift('Fabio') 
+// console.log(alunos)
+
+//                0         1       2
+// const alunos = ['Luiz', 'Maria', 'João']
+// alunos.pop() Removendo um valor do array mas do final
+// const removido = alunos.pop() Salvando o valor que foi removido em uma variavel
+// console.log(alunos)
+// console.log(removido)
+// alunos.shift() //Rmovendo do começo
+// console.log(alunos)
+// const removerDoInicio = alunos.shift()
+// console.log(removerDoInicio)
+// console.log(alunos)
+
+//                0         1       2
+// const alunos = ['Luiz', 'Maria', 'João']
+// delete alunos[0] //Excluindo o valor e não o seu indice
+// console.log(alunos)
+// console.log(alunos[0])
+
+//                0         1       2
+// const alunos = ['Luiz', 'Maria', 'João']
+// alunos.push('Andrea')
+// alunos.push('Eduardo')
+// console.log(alunos.slice(0,2)) //Fatiando um array
+// console.log(alunos.slice(0,-1)) //Fatiando um array
+
+//                0         1       2
+// const alunos = ['Luiz', 'Maria', 'João']
+// console.log(typeof(alunos))
+// console.log(alunos instanceof(Array)) //Verificando se uma instancia de array
+
+//                0         1       2
+// let alunos = ['Luiz', 'Maria', 'João']
+// alunos = 123 //Mudando todo o valor do array
+// console.log(alunos)
+// console.log(alunos instanceof(Array))
